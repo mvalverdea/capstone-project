5b2bf1e48124560612d37d315f376a42dce1643e
Miguel Valverde <mvalverdea@uni.pe>
2024-09-21 10:05:47 -0500

    first repository

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ConfirmedBooking = () => {
+    return (
+        <div className="confirm">
+            <div>
+                <h1>Your reservation has been <span>confirmed!</span></h1>
+            </div>
+        </div>
+    );
+}
+
+export default ConfirmedBooking;
